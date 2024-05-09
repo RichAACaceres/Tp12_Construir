52359d136874032d521d3ac470ca1e913fef956a
tlncho <martinsuarez811@gmail.com>
2024-05-09 12:33:59 -0300

    Meti todo a metodos

--- src/tp12_construir/Tp12_Construir.java
+++ src/tp12_construir/Tp12_Construir.java
@@ -12,48 +12,79 @@ public class Tp12_Construir {
 
     public static void main(String[] args) {
 
-        try {
-            Class.forName("org.mariadb.jdbc.Driver");
-            System.out.println("Driver cargado");
+        try { 
+            //Metodo para cargar driver
+            cargarDrivers();
+            //Conectar a la bd
             String url="jdbc:mariadb://localhost:3308/construirsa";
             String usuario="root";
             String contraseña="";
             Connection con=DriverManager.getConnection(url,usuario,contraseña);
             System.out.println("Conectado a la base de datos");
-            //agregar 3 empleados
-            String sqlEmple1="INSERT INTO empleado( dni, apellido, nombre, acceso, estado) "
-                    + "VALUES ('25997','Caceres','Ricardo',1,true)";
+           //Metodo para agregar 3 empleados
+            agregarEmpleado(con);
+            System.out.println("---------------------------------");
+          //Metodo para agregar 2 herramientas
+            agregarHerramientas(con);
+            //Metodo para listar herramientas con estock mayor a 10
+             listarherramientas(con);
+               System.out.println("-------------------------------------");
+            //Metodo para dar de baja a un empleado
+            darDeBaja(con);
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(null,"Error a cargar el driver");
+            System.out.println(ex.getMessage());
+           
+        } catch (SQLException ex) {JOptionPane.showMessageDialog(null,"Error a cconectarse a la base de datos");
             
-            PreparedStatement ps= con.prepareStatement(sqlEmple1);
-            ps.executeUpdate();
-            
-            
-            String sqlEmple2="INSERT INTO empleado( dni, apellido, nombre, acceso, estado) "
-                    + "VALUES ('25033','Caceres','Alberto',1,true)";
-            
-            PreparedStatement ps1= con.prepareStatement(sqlEmple2);
-            ps1.executeUpdate();
-            
-            String sqlEmple3="INSERT INTO empleado( dni, apellido, nombre, acceso, estado) "
-                    + "VALUES ('25000','Mamani','Susana',1,true)";
-            
-            PreparedStatement ps2= con.prepareStatement(sqlEmple3);
-            ps2.executeUpdate();
-            
-            //agregar 2 herramientas
-            String sqlH="INSERT INTO herramienta(nombre, descripcion, stock, estado)" 
-                    +"VALUES ('Taladro','con percutor','13',true)";
-            
-            PreparedStatement psh= con.prepareStatement(sqlH);
+            System.out.println(ex.getMessage());
+        }
+    
+    }
+    public static void cargarDrivers() throws ClassNotFoundException{
+    Class.forName("org.mariadb.jdbc.Driver");
+            System.out.println("Driver cargado");
+   }
+   
+  public static void agregarEmpleado(Connection con) throws SQLException{
+     //Empleado 1
+      String emp1="INSERT INTO empleado( dni, apellido, nombre, acceso, estado) "
+              + "VALUES (23542345,'Sosa','Rogelio',4,1)";
+      PreparedStatement ps=con.prepareStatement(emp1);
+      ps.executeUpdate();
+      
+      //Empleado 2
+      String emp2="INSERT INTO empleado( dni, apellido, nombre, acceso, estado) "
+              + "VALUES (23542345,'Sosa','Rogelio',4,1)";
+      PreparedStatement ps2=con.prepareStatement(emp2);
+     ps.executeUpdate();
+     //Empleado 3
+     String emp3="INSERT INTO empleado( dni, apellido, nombre, acceso, estado) "
+              + "VALUES (23542345,'Sosa','Rogelio',4,1)";
+      PreparedStatement ps3=con.prepareStatement(emp2);
+     int verificar=ps.executeUpdate();
+     if(verificar>0){
+         System.out.println("Empleados agregados");
+     }
+     
+  }
+  public static void agregarHerramientas(Connection con) throws SQLException{
+  String sqlH="INSERT INTO herramienta(nombre, descripcion, stock, estado)" 
+                   +"VALUES ('Taladro','con percutor','13',true)";
+           
+          PreparedStatement psh= con.prepareStatement(sqlH);
             psh.executeUpdate();
             
             String sqlH1="INSERT INTO herramienta(nombre, descripcion, stock, estado)" 
                     +"VALUES ('pala','ancha','113',true)";
             
             PreparedStatement psh1= con.prepareStatement(sqlH1);
-            psh1.executeUpdate();
-            //Listar herramientas con estock mayor a 10
-            String lista="SELECT * FROM herramienta WHERE stock > 10";
+            int ver=psh1.executeUpdate();
+            if(ver>0){
+                System.out.println("Herramientas agregadas");}
+  }
+  public static void listarherramientas(Connection con) throws SQLException{
+      String lista="SELECT * FROM herramienta WHERE stock > 10";
             PreparedStatement lisherr= con.prepareStatement(lista);
             
             ResultSet datos= lisherr.executeQuery();
@@ -71,16 +102,16 @@ public class Tp12_Construir {
                 System.out.println("Estado : "+estado);
                 System.out.println("----------------------------------------");
             }
-            
-            
-        } catch (ClassNotFoundException ex) {
-            JOptionPane.showMessageDialog(null,"Error a cargar el driver");
-            System.out.println(ex.getMessage());
-           
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null,"Error a cconectarse a la base de datos");
-            System.out.println(ex.getMessage());
-        }
-    }
-    
+  
+  
+  }
+ public static void darDeBaja(Connection con) throws SQLException{
+  String baja="UPDATE empleado SET estado = 0 WHERE idEmpleado = 1";
+            PreparedStatement ps=con.prepareStatement(baja);
+            int cont= ps.executeUpdate();
+            if(cont>0){
+                System.out.println("Empleado dado de baja");
+            }
+ }
+ 
 }
